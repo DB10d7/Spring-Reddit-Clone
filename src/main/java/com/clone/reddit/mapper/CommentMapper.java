f19198b56398aa167b38d117738e2ae0a03eac8a
piofxdev1 <piofxdev1@gmail.com>
2021-09-28 17:53:27 +0530

    register,login,subreddit,posts and comments

---
+++ src/main/java/com/clone/reddit/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.clone.reddit.mapper;
+
+
+import com.clone.reddit.DTO.CommentsDto;
+import com.clone.reddit.model.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    @Mapping(target = "user", source = "user")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
